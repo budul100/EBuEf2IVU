3df0d9f3c2ed9f1a459b1a0bda953b0ad8eab280
budul <marekgrochowski@web.de>
2019-09-26 21:01:50 +0100

    Linux compiling changed to ARM

--- Create_Linux.bat
+++ Create_Linux.bat
@@ -1,11 +1,12 @@
-SET FILENAME=EBuEf2IVUCore.*.*.*.linux-x64.deb
-SET SOURCEDIR=.\EBuef2IVUCore\bin\Release\netcoreapp2.2\linux-x64
+SET FILENAME=EBuEf2IVUCore.*.*.*.linux-arm.deb
+SET SOURCEDIR=.\EBuef2IVUCore\bin\Release\netcoreapp2.2\linux-arm
 SET TARGETDIR=.
 SET DROPBOXDIR=%USERPROFILE%\Dropbox\Public\EBuEf
 
 del /q %TARGETDIR%\%FILENAME%
 del /q %DROPBOXDIR%\%FILENAME%
 
+REM dotnet publish -c Release -r linux-arm -f netcoreapp2.2 --self-contained
 dotnet deb -c Release -r linux-arm -f netcoreapp2.2
 
 xcopy /y %SOURCEDIR%\%FILENAME% %TARGETDIR%
