16b049bd7c70f14adda6a6942ba212a088ae3855
Marek Grochowski <marekgrochowski@web.de>
2019-09-26 15:58:01 +0200

    Track matching changed (more detailed track messages are accepted, too)

--- EBuef2IVUCore/Converters/TrainPositionConverter.cs
+++ EBuef2IVUCore/Converters/TrainPositionConverter.cs
@@ -32,8 +32,8 @@ namespace EBuEf2IVUCore.Converters
 
             var mapping = infrastructureMappings
                 .Where(m => m.MessageBetriebsstelle.IsMatch(message.Betriebsstelle))
-                .Where(m => m.MessageStartGleis.IsMatch(message.StartGleis)
-                    && m.MessageEndGleis.IsMatch(message.EndGleis))
+                .Where(m => m.MessageStartGleis.IsMatchOrEmptyOrDefault(message.StartGleis)
+                    && m.MessageEndGleis.IsMatchOrEmptyOrDefault(message.EndGleis))
                 .OrderByDescending(m => m.MessageStartGleis.IsMatch(message.StartGleis))
                 .ThenByDescending(m => m.MessageEndGleis.IsMatch(message.EndGleis))
                 .FirstOrDefault();
