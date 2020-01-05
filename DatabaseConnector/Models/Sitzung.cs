dd3d7345dbc42afda583c0b1e5cb32322c9b9022
budul <marekgrochowski@web.de>
2020-01-05 18:24:33 +0100

    Determination of session data implemented

---
+++ DatabaseConnector/Models/Sitzung.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatabaseConnector.Models
+{
+    [Table("fahrplan_session")]
+    internal class Sitzung
+    {
+        #region Public Properties
+
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("sim_ivutag")]
+        public DateTime? IvuDate { get; set; }
+
+        #endregion Public Properties
+    }
+}
