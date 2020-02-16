b04ef3f36e79dcf9d9a45eaa9d4d3a13e69d9285
Marek Grochowski <marekgrochowski@web.de>
2020-02-16 15:46:04 +0000

    EBuEf2IVUCrew added

---
+++ EBuEf2IVUCrew/Settings/EBuEfDBConnector.cs
@@ -0,0 +1,13 @@
+﻿namespace EBuEf2IVUVehicle.Settings
+{
+    internal class EBuEfDBConnector
+    {
+        #region Public Properties
+
+        public string ConnectionString { get; set; }
+
+        public int RetryTime { get; set; }
+
+        #endregion Public Properties
+    }
+}
---
+++ EBuEf2IVUCrew/Worker.cs
@@ -0,0 +1,82 @@
+﻿using Common.Interfaces;
+using DatabaseConnector;
+using EBuEf2IVUVehicle.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EBuEf2IVUCrew
+{
+    internal class Worker
+        : BackgroundService
+    {
+        #region Private Fields
+
+        private readonly IConfiguration config;
+        private readonly ILogger<Worker> logger;
+        private IConnector databaseConnector;
+        private CancellationTokenSource sessionCancellationTokenSource;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public Worker(IConfiguration config, ILogger<Worker> logger)
+        {
+            this.config = config;
+            this.logger = logger;
+        }
+
+        #endregion Public Constructors
+
+        #region Protected Methods
+
+        protected override async Task ExecuteAsync(CancellationToken workerCancellationToken)
+        {
+            while (!workerCancellationToken.IsCancellationRequested)
+            {
+                var sessionCancellationToken = GetSessionCancellationToken(workerCancellationToken);
+                databaseConnector = GetConnector(sessionCancellationToken);
+
+                await TestAsync();
+            }
+        }
+
+        #endregion Protected Methods
+
+        #region Private Methods
+
+        private IConnector GetConnector(CancellationToken sessionCancellationToken)
+        {
+            var settings = config
+                .GetSection(nameof(EBuEfDBConnector))
+                .Get<EBuEfDBConnector>();
+
+            logger.LogDebug($"Verbindung zur Datenbank herstellen: {settings.ConnectionString}.");
+
+            var result = new Connector(
+                logger: logger,
+                connectionString: settings.ConnectionString,
+                retryTime: settings.RetryTime,
+                cancellationToken: sessionCancellationToken);
+
+            return result;
+        }
+
+        private CancellationToken GetSessionCancellationToken(CancellationToken workerCancellationToken)
+        {
+            sessionCancellationTokenSource = new CancellationTokenSource();
+            workerCancellationToken.Register(() => sessionCancellationTokenSource.Cancel());
+
+            return sessionCancellationTokenSource.Token;
+        }
+
+        private async Task TestAsync()
+        {
+        }
+
+        #endregion Private Methods
+    }
+}
