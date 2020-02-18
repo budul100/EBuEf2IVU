78ec81ddb00a58254c6dccf3a513bcec625a5175
Marek Grochowski <marekgrochowski@web.de>
2020-02-18 22:23:28 +0000

    Service settings updated and renamed

---
+++ EBuEf2IVUCrew/Settings/ServiceSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace EBuEf2IVUCrew.Settings
+{
+    internal class ServiceSettings
+    {
+        #region Public Properties
+
+        public int AbfrageIntervalSek { get; set; }
+
+        public int AbfrageInVergangenheitMin { get; set; }
+
+        public int AbfrageInZukunftMin { get; set; }
+
+        #endregion Public Properties
+    }
+}
