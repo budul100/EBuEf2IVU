28235d7d5e360f305a8aef283b9a1860398c189d
Marek Grochowski <marekgrochowski@web.de>
2020-02-20 02:59:33 +0000

    Building helpers improved

--- _Helpers/Create_Release.bat
+++ _Helpers/Create_Release.bat
@@ -1,6 +1,9 @@
 pushd ..
 
+dotnet restore .\EBuEf2IVUCrew\EBuEf2IVUCrew.csproj 
 dotnet build -c release -f netcoreapp3.1 .\EBuEf2IVUCrew\EBuEf2IVUCrew.csproj 
+
+dotnet restore .\EBuEf2IVUVehicle\EBuEf2IVUVehicle.csproj 
 dotnet build -c release -f netcoreapp3.1 .\EBuEf2IVUVehicle\EBuEf2IVUVehicle.csproj 
 
 pause
