b79b9afef80331a162b5de11556be4c9f879c309
Marek Grochowski <marekgrochowski@web.de>
2020-10-12 00:08:54 +0200

    Building scripts adjusted

--- Create_Linux-x64.bat
+++ Create_Linux-x64.bat
@@ -7,9 +7,12 @@ SET TARGETDIR=.
 SET SOURCEDIR=.\bin\Release\%FRAMEWORK%\%RUNTIME%
 SET DROPBOXDIR=%USERPROFILE%\Dropbox\Public\EBuEf
 
+echo.
+echo.
 echo ***
 echo *** Clean solution ***
 echo ***
+echo.
 
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
@@ -21,14 +24,21 @@ REM ****************************************************************************
 SET SERVICENAME=EBuEf2IVUCrew
 SET DEBFILE=%SERVICENAME%.*.*.*.%RUNTIME%.deb
 
+echo.
+echo.
 echo ***
 echo *** Build %SERVICENAME% ***
 echo ***
+echo.
 
 dotnet restore .\Programs\%SERVICENAME%\%SERVICENAME%.csproj 
+echo.
+
 dotnet build -c release -f %FRAMEWORK% .\Programs\%SERVICENAME%\%SERVICENAME%.csproj 
+echo.
 
 BASH -c "sh Create_Linux-x64.sh %SERVICENAME%"
+echo.
 
 DEL /q %DROPBOXDIR%\%DEBFILE%
 
@@ -40,14 +50,21 @@ REM ****************************************************************************
 SET SERVICENAME=EBuEf2IVUPath
 SET DEBFILE=%SERVICENAME%.*.*.*.%RUNTIME%.deb
 
+echo.
+echo.
 echo ***
 echo *** Build %SERVICENAME% ***
 echo ***
+echo.
 
 dotnet restore .\Programs\%SERVICENAME%\%SERVICENAME%.csproj 
+echo.
+
 dotnet build -c release -f %FRAMEWORK% .\Programs\%SERVICENAME%\%SERVICENAME%.csproj 
+echo.
 
 BASH -c "sh Create_Linux-x64.sh %SERVICENAME%"
+echo.
 
 DEL /q %DROPBOXDIR%\%DEBFILE%
 
@@ -59,14 +76,21 @@ REM ****************************************************************************
 SET SERVICENAME=EBuEf2IVUVehicle
 SET DEBFILE=%SERVICENAME%.*.*.*.%RUNTIME%.deb
 
+echo.
+echo.
 echo ***
 echo *** Build %SERVICENAME% ***
 echo ***
+echo.
 
 dotnet restore .\Programs\%SERVICENAME%\%SERVICENAME%.csproj 
+echo.
+
 dotnet build -c release -f %FRAMEWORK% .\Programs\%SERVICENAME%\%SERVICENAME%.csproj 
+echo.
 
 BASH -c "sh Create_Linux-x64.sh %SERVICENAME%"
+echo.
 
 DEL /q %DROPBOXDIR%\%DEBFILE%
 
