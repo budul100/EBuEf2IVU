e22d1f440a65a8cda9b79be94fbdb9dd96101308
Marek Grochowski <marekgrochowski@web.de>
2020-10-13 00:03:42 +0200

    Converter split from base class

--- Services/TrainPathSender/Sender.cs
+++ Services/TrainPathSender/Sender.cs
@@ -4,7 +4,6 @@ using Common.Extensions;
 using Common.Interfaces;
 using Common.Models;
 using CredentialChannelFactory;
-using DateTimeExtensions;
 using EnumerableExtensions;
 using Microsoft.Extensions.Logging;
 using Polly;
@@ -16,6 +15,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using TrainPathImportService;
+using TrainPathSender.Converters;
 
 namespace TrainPathSender
 {
@@ -24,22 +24,12 @@ namespace TrainPathSender
     {
         #region Private Fields
 
-        private const string SingleDayBitmask = "1";
-        private const string TimetableVersionId = "Timetable";
-
         private readonly ConcurrentQueue<importTrainPaths> importsQueue = new ConcurrentQueue<importTrainPaths>();
         private readonly ILogger<Sender> logger;
 
         private Factory<TrainPathImportWebFacadeChannel> channelFactory;
-        private string importProfile;
-        private string infrastructureManager;
-        private string orderingTransportationCompany;
+        private TrainRuns2ImportPaths converter;
         private AsyncRetryPolicy retryPolicy;
-        private DateTime sessionDate;
-        private string stoppingReasonPass;
-        private string stoppingReasonStop;
-        private TimetableVersion timetableVersion;
-        private TrainPathKeyTimetableVersion timetableVersionKey;
         private string trainPathState;
 
         #endregion Private Fields
@@ -59,7 +49,9 @@ namespace TrainPathSender
         {
             if (trainRuns.AnyItem())
             {
-                var imports = GetImportTrainPaths(trainRuns);
+                var imports = converter.Get(
+                    trainRuns: trainRuns,
+                    trainPathState: trainPathState);
 
                 if (imports != default)
                     importsQueue.Enqueue(imports);
@@ -70,17 +62,15 @@ namespace TrainPathSender
             int retryTime, DateTime sessionDate, string infrastructureManager, string orderingTransportationCompany,
             string trainPathState, string stoppingReasonStop, string stoppingReasonPass, string importProfile)
         {
-            this.sessionDate = sessionDate;
-            this.infrastructureManager = infrastructureManager;
-            this.orderingTransportationCompany = orderingTransportationCompany;
-
             this.trainPathState = trainPathState;
-            this.stoppingReasonStop = stoppingReasonStop;
-            this.stoppingReasonPass = stoppingReasonPass;
-            this.importProfile = importProfile;
 
-            timetableVersion = GetTimetableVersion();
-            timetableVersionKey = GetTimetableVersionKey();
+            converter = new TrainRuns2ImportPaths(
+                sessionDate: sessionDate,
+                infrastructureManager: infrastructureManager,
+                orderingTransportationCompany: orderingTransportationCompany,
+                stoppingReasonStop: stoppingReasonStop,
+                stoppingReasonPass: stoppingReasonPass,
+                importProfile: importProfile);
 
             channelFactory = new Factory<TrainPathImportWebFacadeChannel>(
                 host: host,
@@ -112,166 +102,6 @@ namespace TrainPathSender
         #endregion Public Methods
 
         #region Private Methods
-
-        private importTrainPaths GetImportTrainPaths(IEnumerable<TrainRun> trainRuns)
-        {
-            var result = default(importTrainPaths);
-
-            if (trainRuns.AnyItem())
-            {
-                var stopPoints = GetNetworkPointKeys(trainRuns).ToArray();
-                var trainPaths = GetTrainPaths(trainRuns).ToArray();
-
-                var request = new TrainPathImportRequest
-                {
-                    importProfile = importProfile,
-                    stopPoints = stopPoints,
-                    timetableVersions = timetableVersion.AsArray(),
-                    trainPaths = trainPaths,
-                };
-
-                result = new importTrainPaths
-                {
-                    trainPathImportRequest = request,
-                };
-            }
-
-            return result;
-        }
-
-        private IEnumerable<NetworkPointKey> GetNetworkPointKeys(IEnumerable<TrainRun> trainRuns)
-        {
-            var positions = trainRuns
-                .SelectMany(r => r.Positions)
-                .Distinct().ToArray();
-
-            foreach (var position in positions)
-            {
-                var result = new NetworkPointKey
-                {
-                    abbreviation = position.Betriebsstelle,
-                    id = position.Betriebsstelle,
-                };
-
-                yield return result;
-            }
-        }
-
-        private IEnumerable<string> GetStoppingReasons(TrainPosition position)
-        {
-            var result = position.IstDurchfahrt
-                ? stoppingReasonPass
-                : stoppingReasonStop;
-
-            yield return result;
-        }
-
-        private TimetableVersion GetTimetableVersion()
-        {
-            var validity = new DateInterval
-            {
-                begin = sessionDate,
-                bitmask = SingleDayBitmask,
-                end = sessionDate
-            };
-
-            var result = new TimetableVersion
-            {
-                id = TimetableVersionId,
-                validity = validity,
-            };
-            return result;
-        }
-
-        private TrainPathKeyTimetableVersion GetTimetableVersionKey()
-        {
-            var result = new TrainPathKeyTimetableVersion
-            {
-                @ref = timetableVersion.id,
-            };
-
-            return result;
-        }
-
-        private TrainPathKey GetTrainPathKey(TrainRun trainRun)
-        {
-            var result = new TrainPathKey
-            {
-                infrastructureManager = infrastructureManager,
-                timetableVersion = timetableVersionKey,
-                trainPathId = trainRun.Zugnummer,
-            };
-
-            return result;
-        }
-
-        private IEnumerable<TrainPath> GetTrainPaths(IEnumerable<TrainRun> trainRuns)
-        {
-            foreach (var trainRun in trainRuns.IfAny())
-            {
-                var trainPathKey = GetTrainPathKey(trainRun);
-                var trainPathVariants = GetTrainPathVariants(trainRun).ToArray();
-
-                var result = new TrainPath
-                {
-                    importValidityFrame = timetableVersion.validity.AsArray(),
-                    infrastructureManagerTrainPathId = trainRun.Zugnummer,
-                    trainPathKey = trainPathKey,
-                    trainPathVariants = trainPathVariants,
-                };
-
-                yield return result;
-            }
-        }
-
-        private IEnumerable<TrainPathStop> GetTrainPathStops(TrainRun trainRun)
-        {
-            foreach (var position in trainRun.Positions)
-            {
-                var times = new Times
-                {
-                    operationalArrivalTime = position.Ankunft.ToDateTime() ?? DateTime.Now,
-                    operationalArrivalTimeSpecified = position.Ankunft.HasValue,
-                    operationalDepartureTime = position.Abfahrt.ToDateTime() ?? DateTime.Now,
-                    operationalDepartureTimeSpecified = position.Abfahrt.HasValue,
-                };
-
-                var stopPoint = new TrainPathStopStopPoint
-                {
-                    @ref = position.Betriebsstelle,
-                };
-
-                var stoppingReasons = GetStoppingReasons(position).ToArray();
-
-                var result = new TrainPathStop
-                {
-                    arrivalTrack = position.Gleis,
-                    departureTrack = position.Gleis,
-                    running = true,
-                    stoppingReasons = stoppingReasons,
-                    stopPoint = stopPoint,
-                    times = times,
-                };
-
-                yield return result;
-            }
-        }
-
-        private IEnumerable<TrainPathVariant> GetTrainPathVariants(TrainRun trainRun)
-        {
-            var trainPathItinerary = GetTrainPathStops(trainRun).ToArray();
-
-            var result = new TrainPathVariant
-            {
-                orderingTransportationCompany = orderingTransportationCompany,
-                state = trainPathState,
-                trainDescription = trainRun.Bemerkungen,
-                trainPathItinerary = trainPathItinerary,
-                validity = timetableVersion.validity.AsArray(),
-            };
-
-            yield return result;
-        }
 
         private void OnRetry(Exception exception, TimeSpan reconnection)
         {
