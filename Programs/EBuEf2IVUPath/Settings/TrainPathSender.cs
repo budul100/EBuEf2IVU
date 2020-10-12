5a91dab5805eab92b3333a6c0346e755c5235c1f
Marek Grochowski <marekgrochowski@web.de>
2020-10-12 20:16:24 +0200

    Train path sender enhanced

---
+++ Programs/EBuEf2IVUPath/Settings/TrainPathSender.cs
@@ -0,0 +1,33 @@
+﻿namespace EBuEf2IVUPath.Settings
+{
+    public class TrainPathSender
+    {
+        #region Public Properties
+
+        public string Host { get; set; }
+
+        public string ImportProfile { get; set; }
+
+        public string InfrastructureManager { get; set; }
+
+        public string OrderingTransportationCompany { get; set; }
+
+        public string Password { get; set; }
+
+        public string Path { get; set; }
+
+        public int Port { get; set; }
+
+        public int RetryTime { get; set; }
+
+        public string StoppingReasonPass { get; set; }
+
+        public string StoppingReasonStop { get; set; }
+
+        public string TrainPathState { get; set; }
+
+        public string Username { get; set; }
+
+        #endregion Public Properties
+    }
+}
