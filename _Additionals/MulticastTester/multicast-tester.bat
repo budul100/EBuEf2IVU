55f75acfebf59572e3c8285505998195928fcd48
budul100 <MarekGrochowski@web.de>
2022-06-22 23:47:34 +0200

    Multicast test tool added

---
+++ _Additionals/MulticastTester/multicast-tester.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo.
+echo The tool can be found on https://github.com/anastyn/multicast-tester
+echo.
+
+java -jar multicast-tester-1.0-SNAPSHOT.jar --sender --group=224.0.0.8 --port=4454
