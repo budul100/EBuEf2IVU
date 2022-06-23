d9fc80edf69ee3f1a89843ab31eecc9710510fdb
budul100 <MarekGrochowski@web.de>
2022-06-23 15:22:18 +0200

    Different senders for status messages and real-time messages added

--- _Additionals/MulticastTester/multicast-tester.bat
+++ _Additionals/MulticastTester/multicast-tester.bat
@@ -1,7 +1,7 @@
 @echo off
 
-echo.
-echo The tool can be found on https://github.com/anastyn/multicast-tester
-echo.
+REM Real time message sender
+start "Real-time messages" cmd /c "echo. & echo Real time message sender - Source https://github.com/anastyn/multicast-tester & echo. & java -jar multicast-tester-1.0-SNAPSHOT.jar --sender --group=224.0.0.7 --port=4453"
 
-java -jar multicast-tester-1.0-SNAPSHOT.jar --sender --group=224.0.0.8 --port=4454
+REM Status message sender
+echo. & echo Status message sender - Source https://github.com/anastyn/multicast-tester & echo. & java -jar multicast-tester-1.0-SNAPSHOT.jar --sender --group=224.0.0.8 --port=4454
