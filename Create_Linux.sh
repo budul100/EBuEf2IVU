3e508acfd4fd449f25f2e7fd456e9f201376777d
budul <budul100@users.noreply.github.com>
2025-04-03 14:21:59 -0400

    Code tidied

--- Create_Linux.sh
+++ Create_Linux.sh
@@ -7,5 +7,5 @@ dotnet deb --configuration Release --framework $2 --runtime $3
 dotnet msbuild /p:TargetFramework=$2 /p:RuntimeIdentifier=$3 /p:Configuration=Release /t:CreateDeb
 
 # cd /mnt/c/Users/mgr/Entwicklung/EBuEf2IVU
-# 
+#
 # docker buildx build --network=host -t "$1" -f ./Programs/$1/Dockerfile .
